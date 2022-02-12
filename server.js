e4c0d35bc93a55df879d7d9b05cf373a1c460a69
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2022-02-12 13:30:52 -0600

    test

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+// this comment is just used to push again
 
 const PORT = process.env.PORT || 5000;
 
