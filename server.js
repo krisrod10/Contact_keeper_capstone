6399562aec24323848cdf4b1c428f219121112cb
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2022-02-12 13:35:30 -0600

    test4

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
-// this comment is just used to push again
-
+// 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
