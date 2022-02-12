b2a9c507eed2483a9c114a1b214c1c6025b31cc6
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2022-02-12 09:17:09 -0600

    Fun time making this app

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Kris Rodgers Contact App</h1>
+      <p className='my-1'>
+        This is a full stack app made by Kris Rodgers for Austin Coding Acadamey Capstone
+      </p>
+    </div>
+  );
+};
+
+export default About;
